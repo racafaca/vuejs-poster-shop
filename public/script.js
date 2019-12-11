eff79fca7a5e196b44c4b3c8ac633e69d5a80af1
racafaca <rboricic89@gmail.com>
2019-12-11 14:39:29 +0100

    023: Cart items quantity logic

--- public/script.js
+++ public/script.js
@@ -4,23 +4,36 @@ new Vue({
     total: 0,
     items: [
       {
+        id: 1,
         title: 'Item 1'
       },
       {
+        id: 2,
         title: 'Item 2'
       },
       {
+        id: 3,
         title: 'Item 3'
       },
     ],
-    cart: [
-
-    ]
+    cart: []
   },
   methods: {
     addItem: function (index) {
       this.total += 9.99
-      this.cart.push(this.items[index])
+      let item = this.items[index]
+      let found = false
+      this.cart.forEach((cartItem) => {
+        if (cartItem.id === item.id) {
+          cartItem.qty++
+          found = true
+        }
+      })
+      if (found) return
+      this.cart.push({
+        ...item,
+        qty: 1
+      })
     }
   }
 })
