d7f1e159ff40f50538ccc8cb91e876444c6f4726
racafaca <rboricic89@gmail.com>
2019-12-11 13:25:38 +0100

    014: Rendering a list of items

--- public/script.js
+++ public/script.js
@@ -2,6 +2,17 @@ new Vue({
   el: '#app',
   data: {
     total: 0,
+    items: [
+      {
+        title: 'Item 1'
+      },
+      {
+        title: 'Item 2'
+      },
+      {
+        title: 'Item 3'
+      },
+    ],
   },
   methods: {
     addItem: function () {
